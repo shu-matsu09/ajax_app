a1ba8ab15d0d50d8620d4104700fb4c23c1348c9
shu-matsu09 <shusukem1988@gmail.com>
2021-01-22 10:50:56 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   post 'posts', to: 'posts#create'
+  # メモのidを取得。queryパラメーターを使用した場合、/posts/?id=1とリクエストを送ると、
+  # params[:id]にてパラメーターを取得できる。今回はpathパラメーターで、一意の情報であるidを取得する
+  get 'posts/:id', to: 'posts#checked'
 end
